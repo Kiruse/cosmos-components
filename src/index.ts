9762659bb1fb5cc7c681e97bb55371f92b46ca84
kiruse <26940742+Kiruse@users.noreply.github.com>
2025-05-01 18:14:16 +0200

    export formatError method so library consumers may add custom formatters

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { CosmosComponents } from './cosmos-components.js';
 export { modals } from './stories/modals/modals.js';
+export { formatError } from './stories/modals/modal-error.js';
 export * from './stories/Address/address.js';
 export * from './stories/Balance/balance.js';
 export * from './stories/Toast/toast.js';
