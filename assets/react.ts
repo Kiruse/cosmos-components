f7181e39e3631580e9d3b716f874874056024df2
kiruse <26940742+Kiruse@users.noreply.github.com>
2025-07-31 09:21:54 +0200

    should've been declared global, oops also .npmignore tsconfig

--- assets/react.ts
+++ assets/react.ts
@@ -8,20 +8,22 @@ interface ReactIntrinsicProps {
   style?: CSSProperties;
 }
 
-// For older versions of React
-declare namespace JSX {
-  interface IntrinsicElements extends CosmosElements<ReactIntrinsicProps> {}
+declare global {
+  // For older versions of React
+  namespace JSX {
+    interface IntrinsicElements extends CosmosElements<ReactIntrinsicProps> {}
+  }
+
+  // For `jsx: 'preserve'` e.g. Next.js
+  namespace React {
+    namespace JSX {
+      interface IntrinsicElements extends CosmosElements<ReactIntrinsicProps> {}
+    }
+  }
 }
 
 // For React 17+
 declare module 'react/jsx-runtime' {
-  namespace JSX {
-    interface IntrinsicElements extends CosmosElements<ReactIntrinsicProps> {}
-  }
-}
-
-// For `jsx: 'preserve'` e.g. Next.js
-declare namespace React {
   namespace JSX {
     interface IntrinsicElements extends CosmosElements<ReactIntrinsicProps> {}
   }
